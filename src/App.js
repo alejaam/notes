0296ff0a7bfb80cc01ece83784c61df0517b9c72
Alejandro Alamar <ale.jaam7@gmail.com>
2021-05-07 03:01:12 -0500

    Repasando useState

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
+import { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Mensaje from './components/mensaje';
 
 function App() {
+
+  const [nombre, setNombre] = useState('Alejandro');
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <div className="form-group">
+        <label for="name">Nombre</label>
+        <input type="text"
+          class="form-control" name="name" id="name" aria-describedby="helpId" placeholder="Introduce tu nombre" onChange={(e) => setNombre(e.target.value)} />
+      </div>
+      <Mensaje texto={nombre} />
     </div>
   );
 }
---
+++ src/components/mensaje.js
@@ -0,0 +1,5 @@
+export default function Mensaje(props) {
+    return (
+        <div>{props.texto}</div>
+    );
+}
